f60d515a4ba2184cf8ac49d0dd6519fa68faff53
mashtalerleonid <mashtalerleonid@gmail.com>
2021-10-02 17:27:46 +0300

    jjj

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,5 @@
-import user from './data/user.json';
-import statisticalData from './data/statistical-data.json';
-import friends from './data/friends.json';
-import transactions from './data/transaction.json';
-import Profile from 'components/UserProfile/Profile';
-import StatisticsList from 'components/Statistics/StatisticsList';
-import Section from 'components/Section';
-import FriendList from 'components/Friends/FriendList';
-import TransactionTable from 'components/TransactionHistory/TransactionTable';
-
 function App() {
-  return (
-    <div>
-      <Section>
-        <Profile
-          name={user.name}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-      </Section>
-      <Section>
-        <StatisticsList title="Upload stats" stats={statisticalData} />
-      </Section>
-      <Section>
-        <FriendList friends={friends} />
-      </Section>
-      <Section>
-        <TransactionTable items={transactions} />
-      </Section>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/components/Section/Section.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Container = styled.div`
-  margin-top: 30px;
-  padding: 25px 0;
-  display: flex;
-  justify-content: center;
-  background-color: rgb(220, 219, 235);
-`;
